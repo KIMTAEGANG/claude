e080ad2563abe70ec2457f4a14c7201de96fb5f0
KIMTAEGANG <64457893+KIMTAEGANG@users.noreply.github.com>
2026-03-02 17:20:01 +0900

    프로젝트 커밋

---
+++ src/main/java/com/example/cluade/CluadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cluade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluadeApplication.class, args);
+	}
+
+}
